b44d44e04017e9a5444d5d732acb36d8d66f42b4
Geraldo Alves <junior.alves.2007@gmail.com>
2020-08-25 01:49:08 -0300

    add effect shimer loading

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Container } from './styles';
-import LeftColumn from '../LeftColumn';
-import RightColumn from '../RightColumn';
-import MiddleColumn from '../MiddleColumn';
-import MobileHeader from '../MobileHeader';
-import DesktopHeader from '../DesktopHeader';
-import AdBanner from '../AdBanner';
+import { Container } from "./styles";
+import LeftColumn from "../LeftColumn";
+import RightColumn from "../RightColumn";
+import MiddleColumn from "../MiddleColumn";
+import MobileHeader from "../MobileHeader";
+import DesktopHeader from "../DesktopHeader";
+import AdBanner from "../AdBanner";
 
 const Layout: React.FC = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    setTimeout( () => {
+       setIsLoading(true);
+    }, 2000);
+  },[]);
+
   return (
     <Container>
       <MobileHeader />
@@ -17,12 +26,12 @@ const Layout: React.FC = () => {
         <AdBanner />
       </span>
       <main>
-        <LeftColumn />
-        <MiddleColumn />
-        <RightColumn />
+        <LeftColumn isLoading={isLoading} />
+        <MiddleColumn isLoading={isLoading} />
+        <RightColumn isLoading={isLoading} />
       </main>
     </Container>
   );
-}
+};
 
 export default Layout;
